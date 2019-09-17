bc7def42b2eed72157b7be7e660510301f43e875
venemexcoin <venemexcoin@gmail.com>
2019-09-16 22:39:40 -0500

    instalacion de mensajes privados

--- src/sockets.js
+++ src/sockets.js
@@ -1,37 +1,60 @@
 module.exports = function (io) {
 
-    let nicknames = [];
+    let users = {};
 
     io.on('connection', socket => {
         console.log('new user connected');
 
         socket.on('new user', (data, cb) => {
 
-            if (nicknames.indexOf(data) != -1) {
+            if (data in users) {
                 cb(false);
             } else {
                 cb(true);
                 socket.nickname = data;
-                nicknames.push(socket.nickname);
+                users[socket.nickname] = socket; //informacion completa de los datos
                 updateNicknames();
             }
         });
 
-        socket.on('send message', data => {
-            io.sockets.emit('new message', {
-                meg: data,
-                nick: socket.nickname
-            });
+        socket.on('send message', (data, cb) => {
+            var msg = data.trim();
+
+            if (msg.substr(0, 3) === '/w ') {
+                msg = msg.substr(3);
+                const index = msg.indexOf(' ');
+                if (index !== -1) {
+                    var name = msg.substring(0, index);
+                    var msg = msg.substring(index + 1);
+                    if (name in users) {
+                        users[name].emit('whisper', {
+                            msg,
+                            nick: socket.nickname
+                        });
+                    } else {
+                        cb('Error! Please enter a Valid User');
+                    }
+                } else {
+                    cb('Error! Please enter you message');
+                }
+            } else {
+
+                io.sockets.emit('new message', {
+                    msg: data,
+                    nick: socket.nickname
+                });
+            }
+
         });
 
         socket.on('disconnect', data => {
             if (!socket.nickname) return;
-            nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+            delete users[socket.nickname];
             updateNicknames();
         });
 
         function updateNicknames() {
-            io.sockets.emit('usernames', nicknames);
+            io.sockets.emit('usernames', Object.keys(users));
         }
     });
 }
